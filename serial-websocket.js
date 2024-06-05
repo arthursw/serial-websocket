ce56449191384ed3fe08ac430601647e226d7ff0
Arthur Masson <arthur.masson@inria.fr>
2024-06-05 14:57:46 +0200

    fix bug: impossible to disconnect when connection was lost

--- serial-websocket.js
+++ serial-websocket.js
@@ -1,7 +1,4 @@
-var najax = $ = require('najax')
 const WebSocket = require('ws');
-const fs = require('fs');
-const readline = require('readline');
 
 let wssSimulator = null;
 let wssController = null;
@@ -10,85 +7,94 @@ let wsSimulator = null;
 let wsController = null;
 
 var SerialPort = require('serialport');
-var port = null;
-var ports = [];
+var ports = {};
+var availablePorts = [];
 
-send = (ws, type, data)=> {
+send = (ws, type, data, portName)=> {
     if(ws != null) {
-        ws.send( JSON.stringify({type: type, data: data, time: Date.now()}) );
+        ws.send( JSON.stringify({type: type, data: data, time: Date.now(), port: portName}) );
     }
 }
 
 let createPort = (data)=> {
-    
-    console.log('Opening port ' + data.name + ', at ' + data.baudRate)
-    if(ports instanceof Array && !ports.includes(data.name)) {
-        console.error('trying to connect to an unexisting port: ', data.name)
+    let portName = data.name
+    console.log('Opening port ' + portName + ', at ' + data.baudRate)
+    if(availablePorts instanceof Array && !availablePorts.includes(portName)) {
+        console.error('trying to connect to an unexisting port: ', portName)
     }
+
+    let port = new SerialPort(portName, { baudRate: data.baudRate, lock:false }, (err)=>portCreationCallback(portName, err))
+
+    port.on('data', (data)=> onPortData(portName, data))
+
+    port.on('error', (err)=> onPortError(portName, err))
     
-    port = new SerialPort(data.name, { baudRate: data.baudRate, lock:false }, portCreationCallback)
-
-    port.on('data', onPortData)
-
-    port.on('error', onPortError)
+    ports[portName] = port
 }
 
-let portCreationCallback = (err)=> {
+let portCreationCallback = (portName, err)=> {
     if (err) {
         console.log('Error: ', err.message);
-        wsController.send({type: 'error', data: err.message});
+        send(wsController, 'error', `${err.message} on port ${portName}.`, portName)
         return;
     }
-    console.log('Connection established.');
-    send(wsController, 'opened')
+    console.log(`Connection established on port ${portName}.`)
+    send(wsController, 'opened', portName, portName)
 }
 
-let onPortData = (data)=> {
+let onPortData = (portName, data)=> {
     let message = data.toString('utf8')
-    send(wsController, 'data', message)
+    send(wsController, 'data', message, portName)
 }
 
-let onPortError = (err)=> {
-    console.error(err.message)
-    send(wsController, 'error', err.message)
+let onPortError = (portName, err)=> {
+    console.error(portName, err.message)
+    send(wsController, 'error', err.message, portName)
 }
 
-let portWriteCallback = (err)=> {
+let portWriteCallback = (portName, err)=> {
     if (err) {
-        console.log('Error on write: ', err.message);
-        send(wsController, 'error', err.message);
+        console.log('Write error on port', portName, ':', err.message)
+        send(wsController, 'error', err.message, portName)
         return;
     }
 }
 
 let listPorts = ()=> {
-    console.log('List ports...');
+    console.log('List availablePorts...')
     SerialPort.list().then(listPortsCallback)
 };
 
 let listPortsCallback = (data)=> {
-    console.log(data);
+    console.log(data)
     if(data != null) {
         if(data instanceof Array) {
-            ports = data
+            availablePorts = data
+            // Close opened ports which are not findable anymore:
+            for(let openedPort of Object.keys(ports)) {
+                // If the opened port is not in the list: close it (set port[portName] to null)
+                if( availablePorts.findIndex((value, index, obj)=> value.path == openedPort) < 0) {
+                    closePort(openedPort)
+                }
+            }
         }
         send(wsController, 'list', data)
     }
 }
 
-let closePort = ()=> {
-    if(port != null) {
+let closePort = (portName)=> {
+    if(ports[portName] != null) {
         console.log('Closing port...')
-        port.close(closePortCallback)
+        ports[portName].close((result)=>closePortCallback(portName, result))
     } else {
-        console.log('Could not close: no opened serial port.')
+        console.log('Could not close port:', portName, ' is not opened.')
     }
 }
 
-let closePortCallback = (result)=> {
-    console.log('Port closed: ', result)
-    send(wsController, 'closed')
-    port = null
+let closePortCallback = (portName, result)=> {
+    console.log('Port', portName, 'closed: ', result)
+    send(wsController, 'closed', portName, portName)
+    ports[portName] = null
 }
 
 let onControllerConnection = (ws)=> {
@@ -99,335 +105,67 @@ let onControllerConnection = (ws)=> {
     listPorts(ws)
 }
 
-let getPortInfo = ()=> {
-    console.log('port info', port)
+let getPortInfo = (portName)=> {
+    let port = portName == null ? Object.values(ports)[0] : ports[portName]
     return port != null ? {baudRate: port.baudRate, isOpen: port.isOpen, path: port.path} : {}
 }
 
 let onControllerMessage = (message)=> {
     let type = null;
     let data = null;
+    let portName = Object.keys(ports)[0];
     try {
         let json = JSON.parse(message);
         type = json.type;
         data = json.data;
+        portName = json.port;
     } catch (e) {
         console.log(e);
     }
     
     if(type == 'data') {
         
-        // if(writeStream != null) {
-        //     writeStream.write(data);
-        //     return
-        // }
-
         if(wsSimulator != null) {
-            wsSimulator.send(data);
+            wsSimulator.send(data)
             return
         }
 
-        if(port == null) {
-            send(wsController, 'error', 'Could not write data: no opened serial port.');
+        if(Object.keys(ports).length == 0) {
+            send(wsController, 'error', 'Could not write data: no opened serial port.', portName)
             return;
         }
-
-        send(wsController, 'sent', data);
-        port.write(data, portWriteCallback);
-    
-    } else if(type == 'comme-un-dessein-start') {
+        if(ports[portName] == null) {
+            send(wsController, 'error', 'Could not data: port not opened.', portName)
+            return;
+        }
+        send(wsController, 'sent', data, portName)
+        ports[portName].write(data, portWriteCallback)
         
-        startCommeUnDessein(wsController);
-
     } else if(type == 'list') {
-        listPorts(wsController);
+        listPorts(wsController)
     } else if(type == 'is-connected') {
-        send(wsController, port != null ? 'connected' : wsSimulator ? 'connected-to-simulator' : 'not-connected', getPortInfo());
+        send(wsController, ports[portName] != null ? 'connected' : wsSimulator ? 'connected-to-simulator' : 'not-connected', getPortInfo(portName), portName)
     } else if(type == 'open') {
-        
-        if(port != null) {
-            send(wsController, 'already-opened', getPortInfo());
+
+        if(ports[portName] != null) {
+            send(wsController, 'already-opened', getPortInfo(portName), portName)
             return
         }
 
         createPort(data)
 
     } else if(type == 'close') {
-        closePort(wsController);
+        closePort(portName)
     }
 }
 
 let onControllerOpen = (data)=> {
-    console.log('WebSocket opened');
+    console.log('WebSocket opened')
 }
 
 let onControllerClose = (data)=> {
-    wsController = null;
+    wsController = null
 }
-
-// Comme Un Dessein
-
-// const commeundesseinAjaxURL = '/ajaxCallNoCSRF/'
-
-// export class CommeUnDessein {
-
-//     static State = {
-//         NextDrawing: "NextDrawing",
-//         RequestedNextDrawing: "RequestedNextDrawing",
-//         Drawing: "Drawing",
-//         SetStatus: "SetStatus",
-//         RequestedSetStatus: "RequestedSetStatus",
-//     }
-    
-//     static RequestTimeout = 2000
-    
-//     CommeUnDesseinSize = new paper.Size(4000, 3000)
-	
-//     mode = 'CommeUnDessein'
-// 	origin = ''
-// 	secret = '******'
-// 	// currentDrawing: { items: any[], pk: string }
-// 	state = CommeUnDessein.State.NextDrawing
-
-// 	testMode = false
-// 	started = false
-// 	serverMode = true
-// 	timeoutID = null
-
-// 	constructor() {
-// 	}
-
-//     commeUnDesseinToDrawArea(point) {
-//         let drawArea = tipibot.drawArea.bounds
-//         let CommeUnDesseinPosition = new paper.Point(-CommeUnDesseinSize.width/2, -CommeUnDesseinSize.height/2)
-//         const CommeUnDesseinDrawArea = new paper.Rectangle(CommeUnDesseinPosition, CommeUnDesseinSize)
-//         return point.subtract(CommeUnDesseinDrawArea.topLeft).divide(CommeUnDesseinDrawArea.size).multiply(drawArea.size).add(drawArea.topLeft)
-//     }
-    
-//     requestNextDrawing() {
-            
-//         if(this.state != State.NextDrawing) {
-//             console.error('CommeUnDessein trying to request next drawing while not in NextDrawing state')
-//             return
-//         }
-
-//         let args = {
-//             cityName: this.mode, secret: this.secret
-//         }
-//         let functionName = this.testMode ? 'getNextTestDrawing' : 'getNextValidatedDrawing'
-//         let data = {
-//             data: JSON.stringify({ function: functionName, args: args })
-//         }
-//         this.state = State.RequestedNextDrawing
-        
-//         console.log('Request next drawing...')
-
-//         // let url = this.testMode ? 'http://localhost:8000/ajaxCallNoCSRF/' : commeundesseinAjaxURL
-//         let url = this.origin + commeundesseinAjaxURL
-//         // $.ajax({ method: "GET", url: url, data: data, xhrFields: { withCredentials: false }, headers: {'Access-Control-Allow-Origin':true} }).done((results) => {
-    
-//         $.ajax({ method: "POST", url: url, data: data }).done((results) => {
-//             if(this.testMode) {
-//                 console.log(results)
-//             }
-//             if (results.message == 'no path') {
-//                 this.state = State.NextDrawing
-//                 console.log('There are no path to draw. Request next drawing in a few seconds...')
-//                 if(this.started) {
-//                     clearTimeout(this.timeoutID)
-//                     this.timeoutID = setTimeout(() => this.requestNextDrawing(), CommeUnDessein.RequestTimeout)
-//                 }
-//                 return
-//             }
-//             if(this.state != State.RequestedNextDrawing) {
-//                 console.error('CommeUnDessein trying to set to draw while not in RequestedNextDrawing state')
-//                 return
-//             }
-//             this.drawSVG(results)
-//             return
-//         }).fail((results) => {
-//             console.error('getNextValidatedDrawing request failed')
-//             console.error(results)
-//             this.state = State.NextDrawing
-//             if(this.started) {
-//                 clearTimeout(this.timeoutID)
-//                 this.timeoutID = setTimeout(() => this.requestNextDrawing(), CommeUnDessein.RequestTimeout)
-//             }
-//         })
-//     }
-
-//     drawSVG(results) {
-//         if (results.state == 'error') {
-//             console.log(results)
-//             return
-//         }
-//         this.state = State.Drawing
-//         this.currentDrawing = results
-
-//         let drawing = new paper.Group()
-
-//         paper.project.importSVG(results.svg, (item, svg)=> {
-//             if(item.visible == false) {
-//                 console.error('When receiving next validated drawing: while importing SVG: the imported item is not visible: ignore.')
-//                 return
-//             }
-//             for (let path of item.children) {
-
-//                 if(path.className != 'Path') {
-//                     continue
-//                 }
-
-//                 // Ignore anything that humans can't see to avoid hacks
-//                 let strokeColor = path.strokeColor
-//                 if(path.strokeWidth <= 0.2 || path.strokeColor == 'white' || path.strokeColor == null || path.opacity <= 0.1 || strokeColor.alpha <= 0.2 || !path.visible) {
-//                     continue
-//                 }
-
-//                 let controlPath = path.clone()
-
-//                 controlPath.flatten(Settings.plot.flattenPrecision)
-                
-//                 // now that controlPath is flattened: convert in draw area coordinates
-//                 for(let segment of controlPath.segments) {
-//                     segment.point = commeUnDesseinToDrawArea(segment.point)
-//                 }
-//                 drawing.addChild(controlPath)
-//             }
-//             item.remove()
-//             if(SVGPlot.svgPlot != null) {
-//                 SVGPlot.svgPlot.destroy()
-//             }
-//             SVGPlot.svgPlot = new SVGPlot(drawing)
-//             SVGPlot.svgPlot.plot(() => this.setDrawingStatusDrawn(results.pk))
-//         })
-//     }
-
-//     setDrawingStatusDrawn(pk) {
-//         if(visualFeedback.paths.children.length > 0) {
-//             visualFeedback.paths.removeChildren()
-//         }
-
-//         if(this.state != State.Drawing) {
-//             console.error('CommeUnDessein trying to setDrawingStatusDrawn while not in Drawing state')
-//             return
-//         }
-
-//         let args = {
-//             pk: pk,
-//             secret: this.secret
-//         }
-//         let functionName = this.testMode ? 'setDrawingStatusDrawnTest' : 'setDrawingStatusDrawn'
-//         let data = {
-//             data: JSON.stringify({ function: functionName, args: args })
-//         }
-//         this.state = State.RequestedSetStatus
-
-//         if(this.testMode) {
-//             console.log('setDrawingStatusDrawn')
-//         }
-
-//         let url = this.origin + commeundesseinAjaxURL
-//         $.ajax({ method: "POST", url: url, data: data }).done((results) => {
-//             console.log(results)
-//             if(this.testMode) {
-//                 console.log(results)
-//             }
-//             if (results.state == 'error') {
-//                 console.error(results)
-//                 return
-//             }
-//             if(this.state != State.RequestedSetStatus) {
-//                 console.error('CommeUnDessein trying to requestNextDrawing while not in RequestedSetStatus state')
-//                 return
-//             }
-//             this.state = State.NextDrawing
-//             if(this.started) {
-//                 this.requestNextDrawing()
-//             }
-//             return
-//         }).fail((results) => {
-//             console.error('setDrawingStatusDrawn request failed')
-//             console.error(results)
-//             this.state = State.Drawing
-//             if(this.started) {
-//                 this.setDrawingStatusDrawn(pk)
-//             }
-//         })
-//     }
-
-//     // From SVG Plot
-
-// 	itemMustBeDrawn(item) {
-// 		return (item.strokeWidth > 0 && item.strokeColor != null) // || item.fillColor != null
-// 	}
-
-// 	moveTipibotLinear(segment) {
-// 		let point = segment.point
-// 		let minSpeed = 0
-// 		// if(Settings.plot.fullSpeed) {
-// 		// 	minSpeed = speeds[segment.index]
-// 		// 	// let speedRatio = minSpeed / Settings.tipibot.maxSpeed
-// 		// 	// let circle = paper.Path.Circle(point, 4)
-// 		// 	// circle.fillColor = <any> { hue: speedRatio * 240, saturation: 1, brightness: 1 }
-// 		// }
-// 		tipibot.moveLinear(point, minSpeed, Settings.tipibot.drawSpeed, ()=> tipibot.pen.setPosition(point, true, false), false)
-// 	}
-
-// 	plotPath(path) {
-// 		if(path.className != 'Path' || !itemMustBeDrawn(path) || path.segments == null) {
-// 			return
-// 		}
-// 		// let speeds = Settings.plot.fullSpeed ? this.computeSpeeds(path) : null
-
-// 		for(let segment of path.segments) {
-// 			let point = segment.point
-
-// 			if(segment == path.firstSegment) {
-// 				if(!tipibot.lastSentPosition.equals(point)) {
-// 					tipibot.penUp()
-// 					tipibot.moveDirect(point, ()=> tipibot.pen.setPosition(point, true, false), false)
-// 				}
-// 				tipibot.penDown()
-// 			} else {
-// 				// this.moveTipibotLinear(segment, speeds)
-// 				this.moveTipibotLinear(segment)
-// 			}
-// 		}
-// 		if(path.closed) {
-// 			// this.moveTipibotLinear(path.firstSegment, speeds)
-// 			this.moveTipibotLinear(path.firstSegment)
-// 		}
-// 	}
-
-// 	plotCurrentPath() {
-// 		this.plotPath(this.currentPath)
-// 		this.nSegments += this.currentPath.segments.length
-// 		let currentPath = this.currentPath.nextSibling
-// 		if(currentPath != null) {
-// 			let currentColor = this.getColorCSS(this.currentPath.strokeColor)
-// 			let nextColor = this.getColorCSS(currentPath.strokeColor)
-// 			if(currentColor != null && nextColor != null && currentColor != nextColor) {
-// 				let wasPenUp = tipibot.pen.isUp
-// 				tipibot.penUp()
-// 				tipibot.sendChangePen(nextColor, this.currentColorIndex++)
-// 				if(!wasPenUp) {
-// 					tipibot.penDown()
-// 				} 
-// 			}
-// 		}
-// 		this.currentPath = currentPath
-// 	}
-
-// 	plotAll() {
-// 		this.nSegments = 0
-// 		while(this.currentPath != null) {
-// 			this.plotCurrentPath()
-// 		}
-// 		communication.interpreter.startQueue()
-// 	}
-// }
-
-// let commeUnDessein = new CommeUnDessein()
 
 // SIMULATOR
 
